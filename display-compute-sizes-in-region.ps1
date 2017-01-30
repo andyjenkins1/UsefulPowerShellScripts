758e2621e68e4ec0254095da8154b1f31f928afd
Andy Jenkins <andyjenkins1@users.noreply.github.com>
2017-01-30 16:46:01 +0000

    Added two new scripts

---
+++ display-compute-sizes-in-region.ps1
@@ -0,0 +1,7 @@
+﻿### Quick Script to display all the Compute Sizes available in a given location.
+### If using Azure xCLI you can use azure vm sizes --location "North Europe"
+
+
+$resources = Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Compute
+$resources.ResourceTypes.Where{($_.ResourceTypeName -eq 'virtualMachines')}.Locations
+Get-AzureRmVmSize -Location "West Europe" | Sort-Object Name | ft Name, NumberOfCores, MemoryInMB, MaxDataDiskCount -AutoSize
---
+++ display-power-status-all-vms.ps1
@@ -0,0 +1,20 @@
+﻿### Quick Script for checking the power status of all VM's running within all resource groups
+### Need to remove / surpress errors on future release
+
+$RGs = Get-AzureRMResourceGroup
+foreach($RG in $RGs)
+{
+    $VMs = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName
+    foreach($VM in $VMs)
+    {
+        $VMDetail = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName -Name $VM.Name -Status
+        foreach ($VMStatus in $VMDetail.Statuses)
+        { 
+             if($VMStatus.Code -like "PowerState/*")
+            {
+                $VMStatusDetail = $VMStatus.DisplayStatus
+            }
+        }
+        write-output $VM.Name $VMStatusDetail
+    }
+}
